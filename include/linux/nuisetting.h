7f6c9ee9122c79e8e42b3090b87c203d8cf201d8
ngxson <thichthat@gmail.com>
2015-06-11 17:30:35 +0700

    Add brightness tweak

---
+++ include/linux/nuisetting.h
@@ -0,0 +1,35 @@
+/*
+ * include/linux/nuisetting.h
+ *
+ * Copyright (c) 2013, Dennis Rassmann <showp1984@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * SETTINGS FOR NUI KERNEL
+ *
+ */
+
+#ifndef _LINUX_NUISETTING_H
+#define _LINUX_NUISETTING_H
+
+/*
+ * Nui brightness settings
+ * 0 for stock value
+ * 1 for darker
+ * 2 for very dark
+ */
+extern int nbr_switch;
+
+#endif	/* _LINUX_NUISETTING_H */
