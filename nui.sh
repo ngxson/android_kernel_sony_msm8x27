fe74adbff65022d24c15272f016b9117b7e3c2d7
ngxson <thichthat@gmail.com>
2015-07-07 14:23:10 +0700

    Update building script

--- nui.sh
+++ nui.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-rm "/home/ngxson/out/arch/arm/boot/zImage"
+THIS_USER=ngxson
+
+rm "/home/$THIS_USER/out/arch/arm/boot/zImage"
+rm "/home/$THIS_USER/zImage"
 
 export ARCH=arm
-export CROSS_COMPILE="/home/ngxson/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.03/bin/arm-cortex_a15-linux-gnueabihf-"
+export CROSS_COMPILE="/home/$THIS_USER/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.03/bin/arm-cortex_a15-linux-gnueabihf-"
 
-make O="/home/ngxson/out" cyanogenmod_nicki_defconfig
-make O="/home/ngxson/out" -j5
+make O="/home/$THIS_USER/out" cyanogenmod_nicki_defconfig
+make O="/home/$THIS_USER/out" -j5
 
-MODULES_DIR="/home/ngxson/modules"
-OUT_DIR="/home/ngxson/out"
-if [ -a "/home/ngxson/out/arch/arm/boot/zImage" ];
+MODULES_DIR="/home/$THIS_USER/modules"
+OUT_DIR="/home/$THIS_USER/out"
+if [ -a "/home/$THIS_USER/out/arch/arm/boot/zImage" ];
+cp "/home/$THIS_USER/out/arch/arm/boot/zImage" "/home/$THIS_USER/zImage"
 then
 rm -rf $MODULES_DIR
 mkdir $MODULES_DIR
 cd $OUT_DIR
 find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
 cd $MODULES_DIR
-/home/ngxson/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.03/bin/arm-cortex_a15-linux-gnueabihf-strip --strip-unneeded *.ko
-cd "/home/ngxson/nuik"
+/home/$THIS_USER/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.03/bin/arm-cortex_a15-linux-gnueabihf-strip --strip-unneeded *.ko
+cd "/home/$THIS_USER/nuik"
 else
 echo "Failed!"
 fi
