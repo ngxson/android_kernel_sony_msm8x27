199c3bce2f597b90e53b6d98e9acd40208601b19
ngxson <thichthat@gmail.com>
2015-07-17 18:17:28 +0700

    New toolchain

--- nui.sh
+++ nui.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 THIS_USER=ngxson
+GCC_TOOLCHAIN=arm-cortex-linux-gnueabi-linaro_4.9.4-2015.06
 
 rm "/home/$THIS_USER/out/arch/arm/boot/zImage"
 rm "/home/$THIS_USER/zImage"
 
 export ARCH=arm
-export CROSS_COMPILE="/home/$THIS_USER/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.03/bin/arm-cortex_a15-linux-gnueabihf-"
+export CROSS_COMPILE="/home/$THIS_USER/$GCC_TOOLCHAIN/bin/arm-cortex_a15-linux-gnueabihf-"
 
 make O="/home/$THIS_USER/out" cyanogenmod_nicki_defconfig
 make O="/home/$THIS_USER/out" -j5
@@ -20,7 +21,7 @@ mkdir $MODULES_DIR
 cd $OUT_DIR
 find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
 cd $MODULES_DIR
-/home/$THIS_USER/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.03/bin/arm-cortex_a15-linux-gnueabihf-strip --strip-unneeded *.ko
+/home/$THIS_USER/$GCC_TOOLCHAIN/bin/arm-cortex_a15-linux-gnueabihf-strip --strip-unneeded *.ko
 cd "/home/$THIS_USER/nuik"
 else
 echo "Failed!"
