7bdde52f1e592ad83df2667395917616ebd2d2e6
ngxson <thichthat@gmail.com>
2015-09-19 22:02:40 +0700

    Fix cmdline

--- mkboot.sh
+++ mkboot.sh
@@ -3,7 +3,7 @@ THIS_USER="ngxson"
 ZIMAGE_DIR="/home/$THIS_USER/out/arch/arm/boot/zImage"
 RAMDISK_DIR="/home/$THIS_USER/nuik/ramdisk.tar.gz"
 BOOTIMG_OUT_DIR="/home/$THIS_USER/nui.img"
-BOARD_KERNEL_CMDLINE="panic=3 console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1"
+BOARD_KERNEL_CMDLINE="panic=3 console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1"
 
 /home/$THIS_USER/nuik/mkbootimg --kernel "$ZIMAGE_DIR" --ramdisk "$RAMDISK_DIR" --board "" \
 	--cmdline "$BOARD_KERNEL_CMDLINE" --base 0x80200000 --pagesize 4096 \
